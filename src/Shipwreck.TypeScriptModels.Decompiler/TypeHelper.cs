a23d4fa3e617d1f3f9d9c3006a1ca6c305a58a20
pgrho <shipwreck@live.jp>
2017-07-16 23:35:54 +0900

    Improved Generic Cecil type to CLR mapping

--- src/Shipwreck.TypeScriptModels.Decompiler/TypeHelper.cs
+++ src/Shipwreck.TypeScriptModels.Decompiler/TypeHelper.cs
@@ -1,6 +1,8 @@
 using ICSharpCode.NRefactory.CSharp;
 using Mono.Cecil;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Shipwreck.TypeScriptModels.Decompiler
 {
@@ -11,8 +13,35 @@ namespace Shipwreck.TypeScriptModels.Decompiler
 
         public static Type ResolveClrType(this TypeReference type)
         {
+            var git = type as GenericInstanceType;
+
+            if (git != null)
+            {
+                var i = git.FullName.IndexOf('<');
+                if (i >= 0)
+                {
+                    var dfn = git.FullName.Substring(0, i);
+                    var dt = Type.GetType(dfn + "," + type.Scope.ToString(), false, false) ?? Type.GetType(dfn, false, false);
+
+                    if (dt != null)
+                    {
+                        var ps = git.GenericArguments.Select(p => p.ResolveClrType()).ToArray();
+
+                        if (Array.IndexOf(ps, null) < 0)
+                        {
+                            return dt.MakeGenericType(ps);
+                        }
+                    }
+
+                    return null;
+                }
+            }
+
+            // TODO: assembly nameに拡張子がついている場合？？？
+            var an = new AssemblyName(type.Scope.ToString());
+
             var fn = type.FullName.Replace('/', '+');
-            return Type.GetType(fn + "," + type.Scope.ToString(), false, false) ?? Type.GetType(fn, false, false);
+            return Type.GetType(fn + "," + an, false, false) ?? Type.GetType(fn, false, false);
         }
 
         public static bool IsEquivalentTo(this Type clrType, TypeReference cecilType)
